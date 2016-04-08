e6f6f82fa4990db6d277d6ffc321560408434dff
oookko <403758013@qq.com>
2016-04-08 22:21:59 +0800

    fork traceur.js
    this is my first fork project

--- src/traceur.js
+++ src/traceur.js
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+//this is my first fork project
 export * from './util-imports.js';
 export * from './compiler-imports.js';
 export * from './loader/loader-imports.js';
